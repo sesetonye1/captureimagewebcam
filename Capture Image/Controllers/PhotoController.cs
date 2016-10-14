3ad1a9a3a871e0316d14789d3a202f22dc36df76
AppSeSe <fsese@appzonegroup.com>
2016-10-14 12:37:45 +0100

    Add project files.

---
+++ Capture Image/Controllers/PhotoController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capture_Image.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // GET: Photo
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Session["val"] = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Imagename)
+        {
+            ViewBag.pic = "http://localhost:49315/WebImages/" + Session["val"].ToString();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Changephoto()
+        {
+            if (Convert.ToString(Session["val"]) != string.Empty)
+            {
+                ViewBag.pic = "http://localhost:49315/WebImages/" + Session["val"].ToString();
+            }
+            else
+            {
+                ViewBag.pic = "../../WebImages/person.jpg";
+            }
+            return View();
+        }
+        public JsonResult Rebind()
+        {
+            string path = "http://localhost:49315/WebImages/" + Session["val"].ToString();
+            return Json(path, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Capture()
+        {
+            var stream = Request.InputStream;
+            string dump;
+            using (var reader = new StreamReader(stream))
+            {
+                dump = reader.ReadToEnd();
+                DateTime nm = DateTime.Now;
+                string date = nm.ToString("yyyymmddMMss");
+                var path = Server.MapPath("~/WebImages/" + date + "test.png");
+                System.IO.File.WriteAllBytes(path, ConvertStringtoBtyes(dump));
+                ViewData["path"] = date + "test.png";
+                Session["val"] = date + "test.png";
+            }
+            return View("Index");
+        }
+        private byte[] ConvertStringtoBtyes(string strInput)
+        {
+            int numBytes = (strInput.Length) / 2;
+            byte[] bytes = new byte[numBytes];
+            for (int x = 0; x < numBytes; ++x)
+            {
+                bytes[x] = Convert.ToByte(strInput.Substring(x * 2, 2), 16);
+            }
+            return bytes;
+        }
+    }
+}
